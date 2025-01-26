90c4ac29d206e0907663d8d5d6b70b05a949dec8
homexhome <homexhome@proton.me>
2025-01-26 16:19:47 +0300

    as

--- scenes/last_loading_level.gd
+++ scenes/last_loading_level.gd
@@ -1,15 +1,17 @@
 extends Node
 
 func pass_last_level(last_level : String):
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(3.0).timeout
 	var i = 5
 	while i > 0:
 		i -= 1
 		Session.show_ad()
-		$AudioStreamPlayer2.play()
+
 		
-		await get_tree().create_timer(.3).timeout
+		await get_tree().create_timer(1).timeout
 	$AudioStreamPlayer.play()
-	await get_tree().create_timer(.3).timeout
+	await get_tree().create_timer(1).timeout
+	$AudioStreamPlayer2.play()
+	await get_tree().create_timer(0.5).timeout
 	Loader.load_level(last_level)
 		
