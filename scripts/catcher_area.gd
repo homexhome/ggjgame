1f5d4f52467ee884d4c0bdba22b03d502dd9a30b
homexhome <homexhome@proton.me>
2025-01-26 15:54:19 +0300

    a

--- scripts/catcher_area.gd
+++ scripts/catcher_area.gd
@@ -5,4 +5,5 @@ func _ready() -> void:
 
 func _on_body_enter(body):
 	if body is not Player: return
-	
+	var current_level = Session.get_current_level().website_path
+	Loader.load_level("http://world/redroom",0,current_level)
